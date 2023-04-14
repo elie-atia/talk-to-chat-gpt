e6e8e6de7711cdb4b75fd39984cd418061134ba1
elie-atia <elie-atia@hotmail.fr>
2023-04-14 13:08:17 +0300

    feat: add voice-to-text function

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import speech_recognition as sr
 
-r = sr.Recognizer()
+def voice_to_text():
+    r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("Parlez maintenant...")
-    audio = r.record(source, duration=5)
-    print("Enregistrement terminé.")
+    print("Combien de temps voulez vous que l'enregistement dure (en seconde)?")
+    duration = input()
 
-try:
-    text = r.recognize_google(audio, language='fr-FR')
-    print("Texte transcrit: " + text)
-except sr.UnknownValueError:
-    print("Je n'ai pas compris ce que vous avez dit.")
-except sr.RequestError as e:
-    print("Erreur lors de la récupération des résultats depuis Google Speech Recognition service; {0}".format(e))
+    with sr.Microphone() as source:
+        print("Parlez maintenant...")
+        audio = r.record(source, duration=int(duration))
+        print("Enregistrement terminé.")
+
+    try:
+        text = r.recognize_google(audio, language='fr-FR')
+        print("Texte transcrit: " + text)
+    except sr.UnknownValueError:
+        print("Je n'ai pas compris ce que vous avez dit.")
+    except sr.RequestError as e:
+        print("Erreur lors de la récupération des résultats depuis Google Speech Recognition service; {0}".format(e))
+
+
+
+if __name__ == "__main__":
+    voice_to_text()
+
+    
